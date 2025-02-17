2003f79bc13a057ed3e6a013e1357dd86cd1abd3
leo-liuzy <zliu@cs.utexas.edu>
2025-02-17 20:13:59 +0000

    commit before merge

--- run_edit.sh
+++ run_edit.sh
@@ -20,9 +20,9 @@ python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive
 python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt}
 
 
-exp_dir_name=llama3.2-1B_on_musique_p0.5_early
-archive=2025-02-12_20-39-47_2326231633
+# exp_dir_name=llama3.2-1B_on_musique_p0.5_early
+# archive=2025-02-12_20-39-47_2326231633
 
-python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=2doc generation.prompt=${prompt}
+# python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=2doc generation.prompt=${prompt}
 
-python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt}
+# python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt}
