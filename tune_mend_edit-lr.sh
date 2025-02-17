79a86531fc8fb6442bcbdfe582b3ee591b94f376
Zeyu Liu <zliu@cs.utexas.edu>
2025-02-17 14:46:49 -0600

    tune hypers

---
+++ tune_mend_edit-lr.sh
@@ -0,0 +1,9 @@
+export CUDA_VISIBLE_DEVICES=5
+
+# python -m run +alg=mend +experiment=musique_dropout +model=llama3.2-1B edit_input=2doc two_doc_at_same_time=True doc_dropout=0.25
+
+for edit_lr in 1e-5 1e-7 1e-4 1e-8
+
+python -m run +alg=mend +experiment=musique +model=llama3.2-1B edit_input=2doc two_doc_at_same_time=True edit_lr=$edit_lr
+
+done
