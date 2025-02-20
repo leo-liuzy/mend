b8dc25b4be658f664e60db9f2446231190f9ab83
Zeyu Liu <zliu@cs.utexas.edu>
2025-02-20 05:29:47 -0600

    running various edits

--- run_edit.sh
+++ run_edit.sh
@@ -1,17 +1,17 @@
-export CUDA_VISIBLE_DEVICES=1
+export CUDA_VISIBLE_DEVICES=3
 
 n_val=1000
 task=musique
 # task=zsre
 # archive=2025-02-10_08-19-14_2641409766
-archive=2025-02-17_16-06-54_298004526
+archive=2025-02-11_23-27-06_6306217737
 
-exp_dir_name=llama3.2-1B_on_zsre-14K #
+exp_dir_name=llama3.2-1B_on_musiqueQonly #
 # prompt=no
 prompt=no
 
 # sft(q_p, a_p)
-python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=sft edit_input=question generation.prompt=${prompt} +do_generation=True
+# python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=sft edit_input=question generation.prompt=${prompt} +do_generation=True
 
 # clm(q_p + a_p)
 python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=question generation.prompt=${prompt} +do_generation=True
--- run_edit_edit-lr.sh
+++ run_edit_edit-lr.sh
@@ -3,23 +3,23 @@ export CUDA_VISIBLE_DEVICES=6
 n_val=1000
 task=musique
 # task=zsre
-archive=2025-02-17_15-56-35_0163092923
+archive=2025-02-18_15-01-06_0891593193
 
-exp_dir_name="llama3.2-1B_on_musique_editlr1e-5" #
+exp_dir_name="llama3.2-1B_on_musique_editlr1e-8" #
 # prompt=no
 prompt=no
 
 # clm(x_i + x_j)
-python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=2doc generation.prompt=${prompt} +do_generation=True
+# python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=2doc generation.prompt=${prompt} +do_generation=True
 
 # clm(x_i)
-python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt} +do_generation=True
+# python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt} +do_generation=True
 
 # 
 # python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=sft edit_input=question generation.prompt=${prompt}
 
 # python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=question generation.prompt=${prompt}
-
+ 
 # python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=2doc generation.prompt=${prompt}
 
 # python run_mend_edit.py +alg=mend +experiment=${task} +model=llama3.2-1B archive=${archive} eval_only=True generation.save_dir=exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=1doc generation.prompt=${prompt}
