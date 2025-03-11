26c77c953b8ea87190cd29d2e0cbd015bd8c574a
Zeyu Liu <zliu@cs.utexas.edu>
2025-03-11 16:18:52 -0500

    update more baseline results

--- train_mend.sh
+++ train_mend.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=5
+export CUDA_VISIBLE_DEVICES=3
 
 # python -m run +alg=mend +experiment=musique_injector +model=llama3.2-1B-eos-sft add_icl=True
 # python -m run +alg=mend +experiment=musique_dropout_better +model=llama3.2-1B-instruct edit_input=2doc two_doc_at_same_time=True doc_dropout=0 add_icl=False
@@ -7,7 +7,7 @@ export CUDA_VISIBLE_DEVICES=5
 
 # python -m run +alg=mend +experiment=musique_dropout +model=llama3.2-1B-eos-sft edit_input=2doc two_doc_at_same_time=True doc_dropout=1
 
-python -m run +alg=mend +experiment=musique_combiner_text +model=llama3.2-1B-eos-sft edit_input=all outer_loop_add_atomq=True lr=1e-4
+python -m run +alg=mend +experiment=musique_combiner_text +model=llama3.2-1B-eos-sft edit_input=all outer_loop_add_atomq=True
 
 # python -m run +alg=mend +experiment=musique_propagator_text +model=llama3.2-1B-eos-sft edit_input=hidden outer_loop_add_atomq=True
 
