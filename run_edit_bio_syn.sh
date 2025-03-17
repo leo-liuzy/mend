91ad1376881da154fd371ad5a073acfa62907d2d
Zeyu Liu <zliu@cs.utexas.edu>
2025-03-17 01:05:48 -0500

    update eval code to run mend model on bio syn data

---
+++ run_edit_bio_syn.sh
@@ -0,0 +1,22 @@
+export CUDA_VISIBLE_DEVICES=0
+
+declare -A name2id=(
+    # metatrain debug on date data
+    [common-dates_1K]=2025-03-15_18-54-39_5970343765
+    [common-dates_1K_noshare]=2025-03-15_18-59-31_7549442309
+    
+    [common-dates_10K]=2025-03-15_18-55-51_1348523953
+
+    [common-dates_100K]=2025-03-15_18-56-25_1434749561
+    [common-dates_100K_noshare]=2025-03-15_18-57-35_6230462214
+)
+
+
+n_val=100
+prompt=no
+task=bio_syn
+
+exp_dir_name=common-dates_100K_noshare
+archive=${name2id[$exp_dir_name]}
+
+python run_mend_edit_bio_syn.py +alg=mend +experiment=${task} +model=llama3.2-1B-common-date-eos-sft archive=${archive} eval_only=True generation.save_dir=debug_exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=seen generation.prompt=${prompt} +do_generation=True +add_bos=True +add_eos=True +add_eos_accuracy=True +gen_w_bos=True +add_icl=False mend.shared=False
