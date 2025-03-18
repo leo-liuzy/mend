a23eb26618e5c49bee19f5118041e56a058a5f6d
leo-liuzy <zliu@cs.utexas.edu>
2025-03-18 21:59:27 +0000

    update from fuji before merging

--- run_edit_bio_syn.sh
+++ run_edit_bio_syn.sh
@@ -19,4 +19,4 @@ task=bio_syn
 exp_dir_name=common-date-year-after_1K_noshare
 archive=${name2id[$exp_dir_name]}
 
-python run_mend_edit_bio_syn.py +alg=mend +experiment=${task} +model=llama3.2-1B-common-date-eos-sft archive=${archive} eval_only=True generation.save_dir=debug_exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=seen generation.prompt=${prompt} +do_generation=True +add_bos=True +add_eos=True +add_eos_accuracy=True +gen_w_bos=True +add_icl=False +spec_question=False +date_data=n mend.shared=False
+python run_mend_edit_bio_syn.py +alg=mend +experiment=${task} +model=llama3.2-1B-common-date-year-after-eos-sft archive=${archive} eval_only=True generation.save_dir=debug_exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=seen generation.prompt=${prompt} +do_generation=True +add_bos=True +add_eos=True +add_eos_accuracy=True +gen_w_bos=True +add_icl=False +spec_question=False +date_data=n mend.shared=False
