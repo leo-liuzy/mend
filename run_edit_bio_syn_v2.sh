1847489b850932a63a9b0b08e82b510a3fcf29a2
leo-liuzy <zliu@cs.utexas.edu>
2025-03-23 03:23:25 +0000

    update eval script for bio_syn_v2 data before merging

--- run_edit_bio_syn_v2.sh
+++ run_edit_bio_syn_v2.sh
@@ -16,14 +16,14 @@ declare -A name2id=(
 )
 
 
-n_val=1
+n_val=100
 prompt=no
 task=bio_syn_v2
 
-exp_dir_name=common-date-year-after_3K_heavy_share_top3
+exp_dir_name=common-date-year-after_3K_heavy_noshare_top3
 archive=${name2id[$exp_dir_name]}
 
 for date_data in all_propagation
 do
-python run_mend_edit_bio_syn_v2.py +alg=mend +experiment=${task} +model=llama3.2-1B-common-date-year-after-eos-sft archive=${archive} eval_only=True generation.save_dir=debug_exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=seen generation.prompt=${prompt} +do_generation=True +add_bos=True +add_eos=True +add_eos_accuracy=True +gen_w_bos=True +add_icl=False +spec_question=True +date_data=${date_data}
+python run_mend_edit_bio_syn_v2.py +alg=mend +experiment=${task} +model=llama3.2-1B-common-date-year-after-eos-sft archive=${archive} eval_only=True generation.save_dir=debug_exp_output/${exp_dir_name}/${task} val_steps=${n_val} edit_loss=clm edit_input=seen generation.prompt=${prompt} +do_generation=True +add_bos=True +add_eos=True +add_eos_accuracy=True +gen_w_bos=True +add_icl=False +spec_question=True +date_data=${date_data} mend.shared=False
 done
