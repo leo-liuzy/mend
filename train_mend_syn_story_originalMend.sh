ad6c0dc1a0778c295dcc194083caad41c3996c46
Zeyu Liu <zliu@cs.utexas.edu>
2025-05-15 00:57:10 -0500

    commit from mll4

--- train_mend_syn_story_originalMend.sh
+++ train_mend_syn_story_originalMend.sh
@@ -7,6 +7,6 @@ train_set_size=40_000
 # llama3.2-1B-common-date-year-after-eos-sft-mid-upper
 
 
-# python -m run +alg=mend +experiment=syn_story_mend +model=llama3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10-midupper3 val_steps=100 log_interval=10 val_interval=100 early_stop_patience=2000 +train_set_size=${train_set_size} mend.shared=False train_prefix=4K
+python -m run +alg=mend +experiment=syn_story_mend +model=llama3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10-midupper3 val_steps=100 log_interval=10 val_interval=100 early_stop_patience=2000 +train_set_size=${train_set_size} mend.shared=True train_prefix=4K
 
-python -m run +alg=mend +experiment=zsre +model=llama3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10-top3 val_steps=100 log_interval=10 val_interval=100 early_stop_patience=2000 mend.shared=True data.rephrase=True
+# python -m run +alg=mend +experiment=zsre +model=llama3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10-top3 val_steps=100 log_interval=10 val_interval=100 early_stop_patience=2000 mend.shared=True data.rephrase=True
