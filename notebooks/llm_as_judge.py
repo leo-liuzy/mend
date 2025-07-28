c898a6561a13c39c92a17ceec6f0db3d05552680
leo-liuzy <zliu@cs.utexas.edu>
2025-07-28 20:45:53 +0000

    commit fuji before merging from remote

--- notebooks/llm_as_judge.py
+++ notebooks/llm_as_judge.py
@@ -72,7 +72,7 @@ llm_judge = LlmAsJudge(
 
 for fpath in tqdm(
     [
-        "/data/users/zliu/mend/synstory_exp_output/4K_heavy_share_layer4_15/syn_story_n-edit2/mend_eval_loss=clm_input=seen_n=350_prompt=no_w-gen_wo-icl_4K_test_ood-relation-question.xlsx",
+        "/data/users/zliu/mend/synstory_exp_output/4K_heavy_share_layer4_15_instruct/syn_story/mend_eval_loss=clm_input=seen_n=500_prompt=no_w-gen_wo-icl_4K_test_id-question.xlsx",
     ]
 ):
     scored_df = pd.read_excel(fpath)
