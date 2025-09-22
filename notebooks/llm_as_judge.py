9d500c58b3d3ba1904d4fbdbf55370e2134fa89d
leo-liuzy <zliu@cs.utexas.edu>
2025-09-22 04:08:54 +0000

    commit before merge from mll

--- notebooks/llm_as_judge.py
+++ notebooks/llm_as_judge.py
@@ -72,7 +72,7 @@ llm_judge = LlmAsJudge(
 
 for fpath in tqdm(
     [
-        "/home/zliu/zliu/mend/synstory_exp_output/Llama-3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10_active-reading_clm-baseline_lr=0.0001_epoch=4.0_tunable-params=all/all_results_ood-relation.xlsx",
+        "/data/users/zliu/mend/synstory_exp_output/Llama-3.2-1B-eos-sft-template-format-curated-v1-lr2e-6-sample-10_active-reading_clm-baseline_lr=1e-05_epoch=4.0_tunable-params=all/all_results_ood-entity.xlsx",
     ]
 ):
     scored_df = pd.read_excel(fpath)
